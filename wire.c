7156fe587fdc6f78731fac66884b47b0d68cd4ac
Ivan Khoronzhuk <khorivan@gmail.com>
2013-01-02 02:40:00 +0200

    WIRE: Adjust code as in linux kernel
    The code is slightly differs on linux kernel style, so add some correlation.
    
    Signed-off-by: Ivan Khoronzhuk <khorivan@gmail.com>

--- wire.c
+++ wire.c
@@ -2,38 +2,34 @@
 #include "delay.h"
 
 //------------------------------------------------------------------------------
-//������ � �������
+//работа с портами
 //------------------------------------------------------------------------------
 
-#define DQ_DOWN()																\
-{																				\
-	CLRB(WIRE_DQ_PORT, WIRE_DQ_B);												\
-	SETB(WIRE_DQ_DDR, WIRE_DQ_B);												\
+#define DQ_DOWN()\
+{\
+	CLRB(WIRE_DQ_PORT, WIRE_DQ_B);\
+	SETB(WIRE_DQ_DDR, WIRE_DQ_B);\
 }
 
-#define DQ_VOID()																\
-{																				\
-	CLRB(WIRE_DQ_DDR, WIRE_DQ_B);												\
-	CLRB(WIRE_DQ_PORT, WIRE_DQ_B);												\
+#define DQ_VOID()\
+{\
+	CLRB(WIRE_DQ_DDR, WIRE_DQ_B);\
+	CLRB(WIRE_DQ_PORT, WIRE_DQ_B);\
 }
 
-#define DQ_IS_HI								CHKB(WIRE_DQ_PIN, WIRE_DQ_B)
+#define DQ_IS_HI		CHKB(WIRE_DQ_PIN, WIRE_DQ_B)
 
 //------------------------------------------------------------------------------
-//��������
+//задержки
 //------------------------------------------------------------------------------
 
-#define DELAY_RESET_PULSE()						delay_us(481)					//������� reset
-#define DELAY_PRESENCE_CHK()					delay_us(270)					//������� ��� �������� ����������
-#define DELAY_PAST_RESET_RDY()					delay_us(211)					//�������� ����� ������������
+#define DELAY_RESET_PULSE()	delay_us(481)	/* задржка reset */
+#define DELAY_PRESENCE_CHK()	delay_us(270)	/* задржка для проверки соеденения */
+#define DELAY_PAST_RESET_RDY()	delay_us(211)	/* задержка после перезагрузки */
+#define DELAY_START()		delay_us(1)	/* задержка на старте тайм слота */
 
-#define DELAY_START()							delay_us(1)						//�������� �� ������ ���� �����
-#define DELAY_SLOTE()							delay_us(40)					//�������� �� ���� ����
-#define DELAY_RECOV()							delay_us(2)						//�������� �� ��������������, ����� ���� �����
-
-//------------------------------------------------------------------------------
-//����. �������
-//------------------------------------------------------------------------------
+#define DELAY_SLOTE()		delay_us(40)	/* задержка на тайм слот */
+#define DELAY_RECOV()		delay_us(2)	/* задержка на восстановление, после тайм слота */
 
 
 void wire_Ini(void)
@@ -41,33 +37,36 @@ void wire_Ini(void)
 	DQ_VOID();
 }
 
+/*
+ * wire_Start - открывает сеанс связи
+ */
 bool wire_Start(void)
-//---��������� ����� �����
 {
 	DQ_DOWN();
 	DELAY_RESET_PULSE();
 	DQ_VOID();
 	DELAY_PRESENCE_CHK();
-	if (!DQ_IS_HI) return 0;
+	if (!DQ_IS_HI)
+		return 0;
 	DELAY_PAST_RESET_RDY();
 	return 1;
 }
 
+/*
+ * wire_Tx_Byte - отправляет байт
+ */
 void wire_Tx_Byte(uint8_t byte)
-//---���������� ����
 {
-	for (ubase_t nb=0; nb<8; nb++)
-	{
+	for (ubase_t nb=0; nb<8; nb++) {
 		DQ_DOWN();
 		DELAY_START();
-		if (byte & 0x01)
-		{
+
+		if (byte & 0x01) {
 			DQ_VOID();
-		}
-		else
-		{
+		} else {
 			DQ_DOWN();
 		}
+
 		DELAY_SLOTE();
 		DQ_VOID();
 		DELAY_RECOV();
@@ -75,37 +74,45 @@ void wire_Tx_Byte(uint8_t byte)
 	}
 }
 
+/*
+ * wire_Rx_Byte - принимает байт
+ */
 void wire_Rx_Byte(uint8_t* byte)
-//---��������� ����
 {
-	for (ubase_t nb=0; nb<8; nb++)
-	{
+	for (ubase_t nb=0; nb<8; nb++) {
 		*byte >>= 1;
 		DQ_DOWN();
 		DELAY_START();
 		DQ_VOID();
 		DELAY_START();
-		if (DQ_IS_HI) *byte |= 0x80;
-		else *byte |= 0x00;
+
+		if (DQ_IS_HI)
+			*byte |= 0x80;
+		else
+			*byte |= 0x00;
+
 		DELAY_SLOTE();
 		DELAY_RECOV();
 	}
 }
 
+/*
+ * wire_Rx_Bit - принимает бит инфы
+ */
 bool wire_Rx_Bit(void)
-//---��������� ��� ����
 {
 	ubase_t b;
 	DQ_DOWN();
 	DELAY_START();
 	DQ_VOID();
 	DELAY_START();
-	if (DQ_IS_HI) b = 1;
-	else b = 0;
+
+	if (DQ_IS_HI)
+		b = 1;
+	else
+		b = 0;
+
 	DELAY_SLOTE();
 	DELAY_RECOV();
 	return b;
 }
-
-
-
--- wire.h
+++ wire.h
@@ -1,39 +1,35 @@
-/* ��������� 1 wire
- * ��������.....................................................................21.12.2010 14:38:35
+/* Интерфейс 1 wire
+ * обновлен...........................................21.12.2010 14:38:35
 */
 
 #include "pdef.h"
 
 //------------------------------------------------------------------------------
-//�����
+//порты
 //------------------------------------------------------------------------------
 
-#define WIRE_DQ_PORT					PORTC
-#define WIRE_DQ_DDR						DDRC
-#define WIRE_DQ_PIN						PINC
-#define WIRE_DQ_B						2
+#define WIRE_DQ_PORT			PORTC
+#define WIRE_DQ_DDR			DDRC
+#define WIRE_DQ_PIN			PINC
+#define WIRE_DQ_B			2
 
 //------------------------------------------------------------------------------
-//��������
+//commands
 //------------------------------------------------------------------------------
 
-#define WIRE_ROM_SEARCH					0xF0									//����� ���������
-#define WIRE_ROM_READ					0x33									//������ ������ ������ ����������
-#define WIRE_ROM_MATCH					0x55									//��������� � ����������� ����������
-#define WIRE_ROM_SKIP					0xCC									//��������� �� ���� �����������
-#define WIRE_ROM_ALARM_SEARCH			0xEC									//����� ��������� c ������ alarm
-#define WIRE_ROM_OVERDRIVE_SKIP			0x3C									//��������� ��� ����������, ������� ��� ������������, � ���������� �����
-#define WIRE_ROM_OVERDRIVE_MATCH		0x69									//��������� ���������� ���������� � ���������� �����
-
-//------------------------------------------------------------------------------
-//�������
-//------------------------------------------------------------------------------
+#define WIRE_ROM_SEARCH			0xF0	/* поиск устройств */
+#define WIRE_ROM_READ			0x33	/* чтение адреса одного устройства */
+#define WIRE_ROM_MATCH			0x55	/* обращение к конкретному устройству */
+#define WIRE_ROM_SKIP			0xCC	/* обращение ко всем устройствам */
+#define WIRE_ROM_ALARM_SEARCH		0xEC	/* поиск устройств c флагом alarm */
+#define WIRE_ROM_OVERDRIVE_SKIP		0x3C	/* переводит все устройства, которые поддерживают, в ускоренный режим */
+#define WIRE_ROM_OVERDRIVE_MATCH	0x69	/* переводит конкретное устройство в ускоренный режим */
 
 void wire_Ini(void);
-bool wire_Start(void);															//---��������� ����� �����
-void wire_Tx_Byte(uint8_t byte);												//---���������� ����
-void wire_Rx_Byte(uint8_t* byte);												//---��������� ����
-bool wire_Rx_Bit(void);															//---��������� ��� ����
+bool wire_Start(void);
+void wire_Tx_Byte(uint8_t byte);
+void wire_Rx_Byte(uint8_t* byte);
+bool wire_Rx_Bit(void);
 
 
 
